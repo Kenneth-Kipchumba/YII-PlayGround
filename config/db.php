8942fadbc4530a100fe6cc9b240b64932e59ace3
Kenneth Kipchumba <branrono@gmail.com>
2022-10-07 10:30:01 +0300

    Trying to gitignore db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=name_of_your_database',
-    'username' => '',
-    'password' => '',
+    'username' => 'your_beautiful_username',
+    'password' => 'your_super_secret_password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
